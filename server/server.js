3c4996a5b0540d4b8a9a6fdefcc27761815b6069
AK21ER <itsmereka21@gmail.com>
2026-02-14 14:24:46 +0300

    server upd

--- server/server.js
+++ server/server.js
@@ -8,16 +8,13 @@ import connectDB from "./db.js";
 import jobRoutes from "./routes/jobRoutes.js";
 import resumeRoutes from "./routes/resumeRoutes.js";
 
-// Needed for __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -33,12 +30,18 @@ app.get("/", (req, res) => {
   res.send(" CV Processor API is running");
 });
 
-// Error handler
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: err.message || "Server Error" });
 });
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`);
+  });
+};
+
+startServer();
